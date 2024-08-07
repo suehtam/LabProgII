27c2e33cfa712e2066fa960e7abc99172f90b3e1
Matheus Rodrigues Alexandre e Silva <matheus4rodrigues@hotmail.com>
2024-08-07 16:48:54 -0300

    Add files via upload

---
+++ Exemplo02.java
@@ -0,0 +1,35 @@
+package estruturaDeCondicoes;
+
+import java.util.Scanner;
+
+public class Exemplo02 
+{
+	public static void Main(String [] args)
+	{
+		int numero = 10;
+		boolean paremenorque100;
+		boolean paremaiorouiguala100;
+		boolean imparemenorque100;
+		boolean imparemaiorouiguala100;
+		if ((numero % 2 == 0) && (numero < 100))
+		{
+			paremenorque100 = true;
+			System.out.println(numero + "é par e menor que 100");
+		}
+		else if((numero % 2 == 0) && (numero >= 100))
+		{
+			paremaiorouiguala100 = true;
+			System.out.println(numero + "é par e maior ou igual a 100");
+		}
+		else if((numero % 2 == 1) && (numero < 100))
+		{
+			imparemenorque100 = true;
+			System.out.println(numero + "é ímpar e menor que 100");
+		}
+		else if((numero % 2 == 1) && (numero >= 100))
+		{
+			imparemaiorouiguala100 = true;
+			System.out.println(numero + "é impar e maior ou igual a 100");
+		}
+	}
+}
